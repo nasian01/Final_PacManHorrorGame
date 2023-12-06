d8565c10936921dc572e6c798256d0f5949b0a33
Jaden Johnson <146070033+ATacoDev@users.noreply.github.com>
2023-12-05 22:22:56 -0800

    Update ButtonCommand.cs

--- Scripts/ButtonCommand.cs
+++ Scripts/ButtonCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public abstract class ButtonCommand : MonoBehaviour
 {
+    public GameObject cursor;
     public abstract void OnSpacePressed();
 }
 
